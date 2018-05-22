7932fc882476a1c50f1e69814a15f49fa000e65c
Will <39299502+will518@users.noreply.github.com>
2018-05-22 10:36:14 +0100

    Refactoring breadcrumb middleware.

--- app/middleware/breadcrumb.js
+++ app/middleware/breadcrumb.js
@@ -1,13 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 const pages = require('../pages');
 
-const replacements = {
-  accountId: ':accountId',
-  socCode: ':socCode',
-  fromQuery: ':fromQuery',
-  occupationTitle: ':occupationTitle',
-};
-
 const pageToBreadcrumbTrail = {
   [pages.SAVED_ROLES]: [pages.INTRODUCTION, pages.SEARCH],
   [pages.SEARCH]: [pages.INTRODUCTION],
@@ -15,6 +8,13 @@ const pageToBreadcrumbTrail = {
   [pages.OCCUPATION]: [pages.INTRODUCTION, pages.SEARCH, pages.RESULTS],
   [pages.HOW_TO]: [pages.INTRODUCTION, pages.SEARCH, pages.RESULTS, pages.OCCUPATION],
   [pages.NOT_FOUND]: [pages.INTRODUCTION],
+};
+
+const replacements = {
+  accountId: ':accountId',
+  socCode: ':socCode',
+  fromQuery: ':fromQuery',
+  occupationTitle: ':occupationTitle',
 };
 
 const pageToBreadcrumb = {
@@ -38,6 +38,8 @@ const pageToBreadcrumb = {
   [pages.NOT_FOUND]: { title: 'Page not found', link: '/${replacements.accountId}/introduction' },
 };
 
+const cloneBreadcrumb = crumb => Object.assign({}, crumb);
+
 const removeLink = (crumb) => {
   const clonedCrumb = Object.assign({}, crumb);
   delete clonedCrumb.link;
@@ -46,47 +48,45 @@ const removeLink = (crumb) => {
 
 module.exports = (currentPage) =>
   (req, res, next) => {
-    const breadcrumbs = [];
+    const trail = [];
     const breadcrumbTrail = pageToBreadcrumbTrail[currentPage];
 
     if (breadcrumbTrail) {
-      breadcrumbTrail.forEach(page => breadcrumbs.push(Object.assign({}, pageToBreadcrumb[page])));
+      breadcrumbTrail.forEach(page => trail.push(cloneBreadcrumb(pageToBreadcrumb[page])));
 
-      breadcrumbs.push(Object.assign({}, pageToBreadcrumb[currentPage]));
+      trail.push(cloneBreadcrumb(pageToBreadcrumb[currentPage]));
     }
 
     const accountId = req.params && req.params.accountId;
-    if (accountId !== undefined) {
-      breadcrumbs.map(crumb =>
-        Object.assign(crumb, { link: crumb.link.replace(replacements.accountId, accountId) })
-      );
-    }
-
     const fromQuery = req.query && req.query.fromQuery;
-    if (fromQuery !== undefined) {
-      breadcrumbs.map(crumb =>
-        Object.assign(crumb, { link: crumb.link.replace(replacements.fromQuery, fromQuery) })
-      );
-    }
-
     const socCode = req.params && req.params.id;
-    if (socCode !== undefined) {
-      breadcrumbs.map(crumb =>
-        Object.assign(crumb, { link: crumb.link.replace(replacements.socCode, socCode) })
-      );
+    const occupationTitle = req.occupation && req.occupation.title;
+
+    trail.forEach(crumb => {
+      let link = crumb.link;
+      let title = crumb.title;
+
+      if (accountId) {
+        link = link.replace(replacements.accountId, accountId);
+      }
+      if (fromQuery) {
+        link = link.replace(replacements.fromQuery, fromQuery);
+      }
+      if (socCode) {
+        link = link.replace(replacements.socCode, socCode);
+      }
+      if (title) {
+        title = title.replace(replacements.occupationTitle, occupationTitle);
+      }
+
+      Object.assign(crumb, { title, link });
+    });
+
+    if (trail.length !== 0) {
+      const lastCrumb = trail.length - 1;
+      trail[lastCrumb] = removeLink(trail[lastCrumb]);
     }
 
-    const title = req.occupation && req.occupation.title;
-    if (title !== undefined) {
-      breadcrumbs.map(crumb =>
-        Object.assign(crumb, { title: crumb.title.replace(replacements.occupationTitle, title) })
-      );
-    }
-
-    if (breadcrumbs.length !== 0) {
-      breadcrumbs[breadcrumbs.length - 1] = removeLink(breadcrumbs[breadcrumbs.length - 1]);
-    }
-
-    Object.assign(res.locals, { trail: breadcrumbs });
+    Object.assign(res.locals, { trail });
     next();
   };
